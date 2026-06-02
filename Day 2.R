3aeb6dcf7c92e132265824375b2a29930e82d483
srishti-sinha-insights <sinha.srishti2005@gmail.com>
2026-06-02 12:56:36 +0530

    number and logic

---
+++ Day 2.R
@@ -0,0 +1,14 @@
+x=10; y=15
+z= x > y
+z
+class(z)
+# Standard logical operations are & (and), | (or), and ! (negation).
+n = as.character(5.69)
+n
+class(n)
+# Merging the character 
+fname = "Himanshu" ;lname = "Singh"
+paste(fname,lname)
+substr("I am in love with the monster ",start = 3 , stop = 15)
+sub("love","hate","I love the monster")
+
