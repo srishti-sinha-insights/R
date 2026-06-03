3731703c2b98f749ca6a7b83cfbbfda9a72a2404
srishti-sinha-insights <sinha.srishti2005@gmail.com>
2026-06-03 13:10:00 +0530

    R programming

---
+++ Day 3.R
@@ -0,0 +1,23 @@
+m=c(10,5,9)
+b=c(36,45,90)
+c(m,b)
+# Arithmetic usage 
+m*b
+m+b
+m-b
+b/m
+#retrieving the vector 
+v=c("Nm","Mc","Lk","Pr","Nq")
+v[4]
+v[-2]
+v[c(1,4:5)]
+v[c(1,1,4)]
+l=c(TRUE,FALSE,FALSE,TRUE,FALSE)
+v[l]
+#Name Vector 
+n=c("Mary","Jane","Watson")
+names(n)=c("First","Middle","Last")
+n
+n["Middle"]
+n[c("Middle","Last")]
+
